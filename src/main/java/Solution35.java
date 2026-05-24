0789b51a31157bad0ad5e63c33605bbbc64e34ec
Dmitriy Pankratov <7snack7@gmail.com>
2026-05-24 23:01:08 +0700

    solution 58

---
+++ src/main/java/Solution35.java
@@ -0,0 +1,35 @@
+public class Solution35 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,3,5,6};
+        int target1 = 5;
+        int[] nums2 = {1,3,5,6};
+        int target2 = 2;
+        int[] nums3 = {1,3,5,6};
+        int target3 = 7;
+
+        System.out.println(searchInsert(nums1, target1));
+        System.out.println(searchInsert(nums2, target2));
+        System.out.println(searchInsert(nums3, target3));
+    }
+
+
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left<=right) {
+            int mid = left + (right-left)/2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
